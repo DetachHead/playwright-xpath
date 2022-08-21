976f704b354d0d48bd410a29027608ff95e223a0
DetachHead <57028336+DetachHead@users.noreply.github.com>
2022-08-21 23:00:45 +1000

    add pw issue comment

--- src/node/main.ts
+++ src/node/main.ts
@@ -3,6 +3,7 @@ import { join } from 'path'
 import { selectors } from 'playwright'
 
 const selectorEngine: Parameters<typeof selectors.register>[1] = {
+    // https://github.com/microsoft/playwright/issues/16705
     content: `(() => {${readFileSync(
         join(__dirname, '../browser/main.js'),
         'utf8',
