414c77f80206a8e921ac46bdd897f23854bfe7de
DetachHead <detachhead@users.noreply.github.com>
2022-08-22 23:59:37 +1000

    switch to fontoxpath for xpath 3 support

--- tests/spec.ts
+++ tests/spec.ts
@@ -3,20 +3,22 @@ import { test } from '@playwright/test'
 import { selectors } from 'playwright'
 
 test.beforeAll(async () => {
-    await selectors.register('xpath2', selectorEngine)
+    await selectors.register('xpath3', selectorEngine)
 })
 
 test('any node', async ({ page }) => {
     await page.setContent('<div>asdf</div>')
-    await page.waitForSelector('xpath2=//*')
+    await page.waitForSelector('xpath3=//*')
 })
 
 test('node type', async ({ page }) => {
     await page.setContent('<div><span>asdf</span></div>')
-    await page.waitForSelector('xpath2=//span')
+    await page.waitForSelector('xpath3=//span')
 })
 
 test('xpath 2 function', async ({ page }) => {
     await page.setContent('<div><button>Click me</button></div>')
-    await page.click("xpath2=//button[starts-with(., 'Click')]")
+    await page.click("xpath3=//button[starts-with(., 'Click')]")
 })
+
+// TODO: xpath 3 tests
