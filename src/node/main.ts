2ce66796f31603bff436f2fb2d0ad2421cd4eb6b
detachhead <detachhead@users.noreply.github.com>
2025-06-26 16:46:01 +1000

    fix crash when registering selector engine in playwright >=1.53

--- src/node/main.ts
+++ src/node/main.ts
@@ -4,10 +4,13 @@ import { selectors } from 'playwright'
 
 const selectorEngine: Parameters<typeof selectors.register>[1] = {
     // https://github.com/microsoft/playwright/issues/16705
-    content: `(() => {${readFileSync(
-        join(__dirname, '../browser/main.js'),
-        'utf8',
-    )};return module.exports.default})()`,
+    content: `(() => {
+        if (typeof module === 'undefined') {
+            window.module = {exports: {}};
+        }    
+        ${readFileSync(join(__dirname, '../browser/main.js'), 'utf8')};
+        return module.exports.default
+    })()`,
 }
 
 export default selectorEngine
